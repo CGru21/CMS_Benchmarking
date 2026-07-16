1e98e35fd96d3655d1356a9cf1a42f681bddecc9
Charlotte Grunert <cgrunert@uni-potsdam.de>
2026-07-16 12:01:06 +0200

    feat: generate benchmarking candidates

--- WorkflowCandidates/Executables/candidate_workflow_1.sh
+++ WorkflowCandidates/Executables/candidate_workflow_1.sh
@@ -18,35 +18,48 @@ project = Project(path=projectPath)
 originalStructure = project.create.structure.bulk('FeAl', crystalstructure='cesiumchloride', a=2.9, cubic=True)
 originalStructure = originalStructure.repeat([2, 2, 2])
 
-# Calculate chemical potential of Al
-bulk_Al = project.create.structure.bulk('Al', crystalstructure='fcc')
+# Calculate chemical potential of Fe
+bulk_Fe = project.create.structure.bulk('Fe', crystalstructure='bcc')
 calc_potential_job = project.create.job.Gpaw(jobName, delete_existing_job=True)
-calc_potential_job.structure = bulk_Al
+calc_potential_job.structure = bulk_Fe
 calc_potential_job.run()
 chemPotential = calc_potential_job.output.energy_tot[0]
 
-# Create Vacancy Al
-vacancyAl = structure.copy()
-del vacancyAl[position]
-
-# Run Vasp
-calc_energy_job = project.create.job.Vasp(jobName, delete_existing_job=False)
+# Run Gpaw
+calc_energy_job = project.create.job.Gpaw(jobName, delete_existing_job=False)
 calc_energy_job.structure = structure
 calc_energy_job.run()
 energy = calc_energy_job.output.energy_tot[0]
 
-# Relax Structure Vasp
-relax_job = project.create.job.Vasp(jobName, delete_existing_job=False)
+# Create Vacancy Fe
+vacancyFe = structure.copy()
+del vacancyFe[position]
+
+# Create Vacancy Fe
+vacancyFe = structure.copy()
+del vacancyFe[position]
+
+# Relax Structure Sphinx
+relax_job = project.create.job.Sphinx(jobName, delete_existing_job=False)
 relax_job.structure = structure
 relax_job.calc_minimize()
 relax_job.run()
 relaxedStructure = relax_job.get_structure()
 
-# Run Vasp
-calc_energy_job = project.create.job.Vasp(jobName, delete_existing_job=False)
+# Run Gpaw
+calc_energy_job = project.create.job.Gpaw(jobName, delete_existing_job=False)
 calc_energy_job.structure = structure
 calc_energy_job.run()
 energy = calc_energy_job.output.energy_tot[0]
+
+# Calculate lattice constant and bulk modulus
+reference_job = project.create.job.Gpaw('gpaw_job', delete_existing_job=True)
+reference_job.structure = project.create.structure.bulk('FeAl', crystalstructure='cesiumchloride', a=2.9)
+murn_job = project.create.job.Murnaghan('murn_job', delete_existing_job=True)
+murn_job.ref_job = reference_job
+murn_job.run()
+bulk_modulus = murn_job.content['output/equilibrium_bulk_modulus']
+lattice_constant = (murn_job.content['output/equilibrium_volume']) ** (1/3)
 
 # Calculate defect formation energy
 defectFormationEnergy = energyDefect - energyOriginal + chemPotential
@@ -56,4 +69,4 @@ k_B = 0.0000862
 temp = 1000
 concentration_defect = math.exp(-defectFormationEnergy/(k_B * temp))
 
-echo "1. output is: $node-1569910685"
+echo "1. output is: $node-1648954588"echo "2. output is: $node-623419654"echo "3. output is: $node-1424517146"
